cbcb29556cef7ea39b9a897b6665897d463d78ad
Ashwinee Kumar Sethi <ashwineek449@gmail.com>
2024-02-23 13:30:22 +0530

    Upload

---
+++ src/components/Products/ProductCard.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Button from "../Shared/Button";
+
+const ProductCard = ({ data }) => {
+  return (
+    <div className="mb-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 place-items-center">
+        {/* card section */}
+        {data.map((data) => (
+          <div
+            data-aos="fade-up"
+            data-aos-delay={data.aosDelay}
+            className="group"
+            key={data.id}
+          >
+            <div className="relative">
+              <img
+                src={data.img}
+                alt=""
+                className="h-[100%] w-[100%] object-cover rounded-md"
+              />
+              {/* hover button */}
+              <div className="hidden group-hover:flex absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 h-full w-full text-center group-hover:backdrop-blur-sm justify-center items-center duration-200 rounded-md">
+                <Button
+                  text={"Add to cart"}
+                  bgColor={"bg-primary"}
+                  textColor={"text-white"}
+                />
+              </div>
+            </div>
+            <div className="leading-7">
+              <h2 className="text-[17px] font-[400]">{data.title}</h2>
+              <h2 className="text-[20px] font-[700]">${data.price}</h2>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
